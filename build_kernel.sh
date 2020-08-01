5e486b7dd6240aa0bfaf735784d9557a1a48db14
mehanik6 <mehanik67@bk.ru>
2020-08-01 18:14:53 +0300

    first commit

---
+++ build_kernel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export CROSS_COMPILE=/home/mehanik6/lineage-16.0/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+mkdir out
+chmod -R 777 out
+make -C $(pwd) O=out -j5 ARCH=arm64 CROSS_COMPILE=/home/mehanik6/lineage-16.0/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android- KCFLAGS=-mno-android  gto_eur_open_defconfig
+make -C $(pwd) O=out -j5 ARCH=arm64 CROSS_COMPILE=/home/mehanik6/lineage-16.0/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android- DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y 2>&1 | tee build.log
+cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image 
+tools/mkdtimg create out/dtbo.img --page_size=4096 $(find out -name "*.dtbo")
